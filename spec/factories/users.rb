ce6c44e79f498eed6fb1c945c57f39e71304ff9e
清水一也 <shimizukazuya@shimizukazuyanoMacBook-Air.local>
2021-02-20 08:52:58 +0900

    --format documentation追記後のテスト結果画像

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "user#{n}@example.com" }
-    password { "kazuya" }
-    password_confirmation { "kazuya" }
+    password { "password" }
+    password_confirmation { "password" }
   end
 end
